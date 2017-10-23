e290453b816462199d08cb9b674400df0cf39865
Jlinn8 <julilinnehan.com>
2017-10-23 20:49:48 +0000

    populated db with seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Breed Group - Toy
+
+Dog.create(breed: "Chihuahua", size: "Small", weight: "2 - 10 lbs.", energy_needs: "Low", grooming_per_month: "Once", pet_compatibility: "No", feed_cost_weekly: "$10", description: "The Chihuahua dog breed‘s charms include his small size, outsize personality, and variety in coat types and colors. He’s all dog, fully capable of competing in dog sports such as agility and obedience, and is among the top 10 watchdogs recommended by experts. He loves nothing more than being with his people and requires a minimum of grooming and exercise." )
+
+Dog.create(breed: "Boston Terrier", size: "Small", weight: "10 - 25 lbs.", energy_needs: "High", grooming_per_month: "Once", pet_compatibility: "Yes", feed_cost_weekly: "$25", description: "Boston Terriers have been popular since their creation a little more than a century ago. They were originally bred to be fighting dogs, but today, they’re gentle, affectionate companions with tuxedo-like markings that earned them the nickname “American Gentleman.")
+
+Dog.create(breed: "Pomeranian", size: "Small", weight: "3 - 7 lbs.", energy_needs: "Medium", grooming_per_month: "Weekly", pet_compatibility: "No", feed_cost_weekly: "$10", description: "Descended from large sled dog breeds, the now-tiny Pomeranian has a long and interesting history. The foxy-faced dog, nicknamed “the little dog who thinks he can,” is compact, active, and capable of competing in agility and obedience or simply being a family friend.")
+
+# Breed Group - Working
+
+Dog.create(breed: "Boxer", size: "Large", weight: "60 - 70 lbs.", energy_needs: "High", grooming_per_month: "Once", pet_compatibility: "Yes", feed_cost_weekly: "$35+", description: "Boxers were originally bred to be medium-size guard dogs. Today, although they are a part of the AKC’s Working Group, they mostly find homes as loving family companions.")
+
+Dog.create(breed: "Schnauzer", size: "Medium", weight: "30 - 50 lbs.", energy_needs: "High", grooming_per_month: "Weekly", pet_compatibility: "Yes", feed_cost_weekly: "$25", description: "He a dog breed who’s got it all in one small package: intelligence, affection, an extroverted temperament, humor, and a personality that’s twice as big as he is. Throw in that walrus moustache and quivering enthusiasm, and he’ll make you laugh every day. With a Miniature Schnauzer in the house, you’ll never be alone, not even when you go to the bathroom. He’s got personality-plus, and whether he’s bounding around ahead of you or curled up snoozing on your lap, you’ll never be bored with him around.")
+
+Dog.create(breed: "Mastiff", size: "Large", weight: "130- 220 lbs.", energy_needs: "High", grooming_per_month: "Bi-Weekly", pet_compatibility: "No", feed_cost_weekly: "$35+", description:"Bright eyed and bushy tailed, with a face that always seems to be smiling, the Shetland Sheepdog, has long been a family favorite. Not a Miniature Collie but his own distinctive breed, the Sheltie is loyal, funny, and smart. He is also a barker.")
+
+
+# Breed Group - Herding
+Dog.create(breed: "Sheltie", size: "Medium", weight: "15 - 25 lbs.", energy_needs: "Medium", grooming_per_month: "Weekly", pet_compatibility: "Yes", feed_cost_weekly: "$25", description: "Bright eyed and bushy tailed, with a face that always seems to be smiling, the Shetland Sheepdog, has long been a family favorite. Not a Miniature Collie but his own distinctive breed, the Sheltie is loyal, funny, and smart. He is also a barker.")
+
+Dog.create(breed: "Cardigan Welsh Corgi", size: "Small", weight: "25 - 38 lbs.", energy_needs: "High", grooming_per_month: "Weekly", pet_compatibility: "Yes", feed_cost_weekly: "$25", description: "The Cardigan Welsh Corgi is the older of the two Corgi dog breeds, with dogs of this type believed to have existed in Wales for more than 3,000 years. The Cardigan is distinguished by his long tail — like the sleeves of a cardigan sweater — and was used to drive cattle to market. Known as the yard-long dog, the Cardigan is sensible and affectionate. He loves to spend time with his family and is an active, fun-loving playmate for school-age children. The Cardigan’s medium-length coat comes in many colors and patterns, including red, brindle, blue merle and black, usually with white markings.")
+
+Dog.create(breed: "German Shepherd", size: "Large", weight: "75 - 95 lbs.", energy_needs: "High", grooming_per_month: "Once", pet_compatibility: "No", feed_cost_weekly: "$35+", description: "The German Shepherd Dog is one of America’s most popular dog breeds — for good reason. He’s an intelligent and capable working dog. His devotion and courage are unmatched. And he’s amazingly versatile, excelling at most anything he’s trained to do: guide and assistance work for the handicapped, police and military service, herding, search and rescue, drug detection, competitive obedience and, last but not least, faithful companion.")
+
+
+
+
+# Breed Group - Hounds 
+
+Dog.create(breed: "Bloodhound", size: "Large", weight: "80 - 110 lbs.", energy_needs: "High", grooming_per_month: "Weekly", pet_compatibility: "Yes", feed_cost_weekly: "$35+", description: "The Bloodhound’s ancestors were created in medieval France to trail deer and boar. Today, he’s a highly active and intelligent dog breed whose keen sense of smell has found him a special place in law enforcement and search and rescue. His fans love him for his sweet nature and unique appearance.")
+
+Dog.create(breed: "Coonhound", size: "Medium", weight: "45 - 60 lbs.", energy_needs: "High", grooming_per_month: "Bi-Weekly", pet_compatibility: "Yes", feed_cost_weekly: "$25", description: "The Treeing Walker dog breed has a short, clear bark and a passion for the hunt. Some are shy, some are confident, but they’re all hounds — meaning, they need patient training and plenty of exercise.")
+
+Dog.create(breed: "Dashund", size: "Small", weight: "7 - 30 lbs.", energy_needs: "Medium", grooming_per_month: "Bi-Weekly", pet_compatibility: "Yes", feed_cost_weekly: "$25", description: "Dachshunds are scent hound dog breeds who were bred to hunt badgers and other tunneling animals, rabbits, and foxes. Packs of Dachshunds were even used to trail wild boar. Today their versatility makes them excellent family companions, show dogs, and small-game hunters.")
